0f56c257a3f6f371d1612d3edcaad3d8c06fa251
surya <suryapals000@gmail.com>
2021-05-29 20:18:04 +0530

    First change

---
+++ solutions.py
@@ -0,0 +1,38 @@
+
+
+#made a change
+
+def biggest(l):
+    # Problem 1
+    pass
+
+def average(l):
+    # Problem 2
+    pass
+
+def longest(s):
+    # Problem 3
+    pass
+
+def first_space(l):
+    # Problem 4
+    pass
+
+def freq(s):
+    # Problem 5
+    pass
+
+def panagram(s):
+    # Problem 6
+    pass
+
+def abbreviate(s):
+    # Problem 7
+    pass
+
+def split(amount, denominations):
+    # Problem 8
+    pass
+
+
+       
